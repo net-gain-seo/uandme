43a49b05f2c8fe1c231aa870445d996e8ea34490
Corbin Maynard <corbinm@netgainseo.com>
2019-12-23 13:44:55 -0500

    added a 404 page and pagination to the blog

---
+++ 404.php
@@ -0,0 +1,17 @@
+<?php
+    get_header();
+    // $postId = get_the_post_id();
+?>
+
+    <div class="page-content">
+        <div class="container-fluid  page-lead-in"><div class="container">
+            <h1>Page Not Found</h1>
+            <p>The page you are looking for is not here. Look below at our service categories to find what you're looking for.</p>
+            </div>
+        </div>
+
+        <?php echo do_shortcode('[common_element id="7479" title="Cards Row"]'); ?>
+    </div>
+
+<?php
+    get_footer();
